e4d8a433f8716fc284504320d8c3e779fce2dc2d
Pumuckl Random <randompumuckl@gmail.com>
2025-06-22 17:22:31 +0200

    reworked json files cleanup

--- backend/user_data/cleanup_user_data.py
+++ backend/user_data/cleanup_user_data.py
@@ -6,78 +6,44 @@ from backend.config.config import CONFIG
 file_dir = os.path.dirname(os.path.relpath(__file__))
 
 
-def cleanup_dicts(timeout: int = 30, dicts_path: str = 'dicts') -> int:
+def cleanup_files(directory: str, timeout: int = CONFIG.files_timeout) -> int:
     """
-    Cleanup dictionaries older than timeout in days.
+    Cleanup json files older than timeout in days.
 
+    :param directory: directory of the json files
     :param timeout: timeout in days
-    :param dicts_path: path to the dictionaries directory
+
     :return: number of removed files
     """
+    files_removed = 0
+    time_now = time.time()
+    time_limit = time_now - (timeout * 24 * 3600)
+    files_path = os.path.join(file_dir, directory)
     try:
-        files_removed = 0
-        time_now = time.time()
-        time_limit = time_now - (timeout * 24 * 3600)
-        dicts_path = os.path.join(file_dir, dicts_path)
-
-        if not os.path.isdir(dicts_path):
-            logger.info(f'No dictionaries to cleanup at "{dicts_path}"')
+        if not os.path.isdir(files_path):
+            logger.info(f'No json files to cleanup at "{files_path}"')
             return 0
 
-        json_files = [file for file in os.listdir(dicts_path) if file.endswith('.json')]
+        json_files = (file for file in os.listdir(files_path) if file.endswith('.json'))
         for json_file in json_files:
-            json_path = os.path.join(dicts_path, json_file)
+            json_path = os.path.join(files_path, json_file)
             if os.path.isfile(json_path) and os.path.getmtime(json_path) < time_limit:
                 try:
                     os.remove(json_path)
                     files_removed += 1
                 except OSError as exception:
-                    logger.error(f'Error removing file "{json_path}" with exception: {exception}')
-        return files_removed
+                    logger.error(f'Error removing json file "{json_path}" with exception: {exception}')
     except Exception as exception:
-        logger.error(f'Error during dictionary cleanup with exception: {exception}')
-        return 0
-
-
-def cleanup_setts(timeout: int = 30, setts_path: str = 'setts') -> int:
-    """
-    Cleanup settings older than timeout in days.
-
-    :param timeout: timeout in days
-    :param setts_path: path to the settings directory
-    :return: number of removed files
-    """
-    try:
-        files_removed = 0
-        time_now = time.time()
-        time_limit = time_now - (timeout * 24 * 3600)
-        setts_path = os.path.join(file_dir, setts_path)
-
-        if not os.path.isdir(setts_path):
-            logger.info(f'No settings to cleanup at "{setts_path}"')
-            return 0
-
-        json_files = [file for file in os.listdir(setts_path) if file.endswith('.json')]
-        for json_file in json_files:
-            json_path = os.path.join(setts_path, json_file)
-            if os.path.isfile(json_path) and os.path.getmtime(json_path) < time_limit:
-                try:
-                    os.remove(json_path)
-                    files_removed += 1
-                except OSError as exception:
-                    logger.error(f'Error removing file "{json_path}" with exception: {exception}')
-        return files_removed
-    except Exception as exception:
-        logger.error(f'Error during settings cleanup with exception: {exception}')
-        return 0
+        logger.error(f'Error during json files cleanup with exception: {exception}')
+    return files_removed
 
 
 def main() -> None:
     try:
         logger.info('Cleanup user data:')
         logger.info(
-            f'Removed: "{cleanup_dicts(timeout = CONFIG.files_timeout)}" dictionary files'
-            f'Removed: "{cleanup_setts(timeout = CONFIG.files_timeout)}" settings files'
+            f'Removed: "{cleanup_files(directory = "dicts")}" dictionary files'
+            f'Removed: "{cleanup_files(directory = "setts")}" settings files'
         )
     except Exception as exception:
         logger.error(f'Failed to cleanup user data with exception: {exception}")')
